00a3d30b786b73ebcd462f20a6d10ecb87111784
Vicente Obregon <desarrollogis@gmail.com>
2015-02-05 13:51:45 -0700

    initial

---
+++ service.py
@@ -0,0 +1,11 @@
+import time
+import xbmc
+
+if __name__ == '__main__':
+	monitor = xbmc.Monitor()
+	while True:
+		# Sleep/wait for abort for 10 seconds
+		if monitor.waitForAbort(10):
+			# Abort was requested while waiting. We should exit
+			break
+		xbmc.log("hello addon! %s" % time.time(), level=xbmc.LOGDEBUG)
